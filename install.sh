da08fb71ef11675d4baa67f66f375558bb3bb135
Howard Huang <h.huang65@gmail.com>
2021-03-14 14:22:17 -0700

    FIX: Ensure $HOME/.config exists
    This fixes a case where this script fails because the folder to link
    into does not yet exist

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p "${HOME}/.config"
+
 if [ $(uname) = 'Linux' ]; then
   ln -sf "${PWD}/config" "${HOME}/.config/chrome-flags.conf"
   ln -sf "${PWD}/config" "${HOME}/.config/chromium-flags.conf"
